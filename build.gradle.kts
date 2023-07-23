de127839529ee98837944caa11550ac92d32cd94
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-07-23 15:44:19 +0200

    Test S3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 tasks.register("uplift") {
-    dependsOn(gradle.includedBuild("taninim").task(":ascension:uplift"))
+    dependsOn(
+        incl("taninim", ":ascension:uplift")
+    )
+}
+
+tasks.register("taninim") {
+    dependsOn(
+        incl("uplift", ":build"),
+        incl("taninim", ":build")
+    )
 }
 
 tasks.register("hello-web") {
-    dependsOn(gradle.includedBuild("uplift/examples/hello-web").task(":hello-web"))
+    dependsOn(
+        incl("uplift/examples/hello-web", ":hello-web")
+    )
 }
+
+fun incl(composite: String, task: String): TaskReference = gradle.includedBuild(composite).task(task)
