5d9bf263b04a5bfcd5151427c476c33b1b2b577c
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2026-02-18 20:59:52 +0100

    Back up!

--- rugen3/resources/public/js/cljs-runtime/shadow.module.main.append.js
+++ rugen3/resources/public/js/cljs-runtime/shadow.module.main.append.js
@@ -1,4 +1,2 @@
 
-shadow.cljs.devtools.client.env.module_loaded('main');
-
 try { taninim.core.init(); } catch (e) { console.error("An error occurred when calling (taninim.core/init)"); console.error(e); }
